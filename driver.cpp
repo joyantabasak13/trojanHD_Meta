42c64c699574a4888b2216c5eb1df80377a34288
joyantabasak13 <joyantabasak13@gmail.com>
2022-03-18 17:34:06 +0600

    added initial codebase
    Signed-off-by: joyantabasak13 <joyantabasak13@gmail.com>

---
+++ driver.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include "c880.h"
+#include "c2670.h"
+#include "c3540.h"
+#include "c5315.h"
+#include "c7552.h"
+
+int main() {
+    c7552 IC;
+    std::vector<bool> test_vector;
+    for(size_t i=0; i<IC.n_input; i++) {
+        test_vector.push_back(i%2);
+    }
+
+    IC.loadInput(test_vector);
+    IC.propagate();
+    std::vector<bool> output = IC.getOutput();
+    std::vector<bool> node_state = IC.getNodeState();
+
+    for(auto i:output) {
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+
+    for(auto i:node_state) {
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+
+    return 0;
+}
